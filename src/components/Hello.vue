98514b3909674b39df72bb0de6a4855a0809ec0c
陈烨彬 Robin Chen <xianyuxmu@gmail.com>
2017-11-19 19:39:42 +0800

    import `axios`

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,30 +1,36 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button v-on:click="queryLectureEvents">Get Lecture Events</button>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
-  name: 'hello',
+  name: 'hello', // component name
   data () {
     return {
-      msg: 'Welcome to Your Vue.js PWA'
+      msg: 'Welcome to XMU Lecture Web App'
+    }
+  },
+  methods: {
+    queryLectureEvents: (e) => {
+      e.preventDefault()
+      axios.get('https://lecture.xmu.edu.cn/events', {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        crossdomain: true
+      })
+        .then(response => {
+          console.log('response: ', response)
+        })
+        .catch(error => {
+          console.error('error: ', error)
+        })
     }
   }
 }
